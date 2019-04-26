e2a95fd61c9f14f52851fbd947317ba66b26d9d7
John Durham <kilo.kilo.delta8@gmail.com>
2019-04-25 21:59:58 -0500

    Added login/login functionality

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from flask import Flask, request, render_template, url_for, flash, redirect, ses
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import Form, StringField, PasswordField, TextAreaField, validators
 from datetime import datetime
-import myhelpers
+from myhelpers import is_email
 
 
 #app configuration
 app = Flask(__name__)
 app.secret_key = 'NotSSOOs@cr@t_K&Y!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://buildablog:BlogPassWord123@localhost:3306/buildablog'
-
+#app.config['SQLALCHEMY_ECHO'] = True
 
 #init db instance
 db = SQLAlchemy(app)
@@ -22,7 +22,8 @@ class User(db.Model):
     username = db.Column(db.String(60), unique=True)
     password = db.Column(db.String(120))
     #constructor   
-    def __init__(self, username, password):
+    def __init__(self, email, username, password):
+        self.email = email
         self.username = username
         self.password = password
     #represent son....
@@ -84,22 +85,60 @@ def home():
 
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    return render_template('register.html', title='Register', form=form)
+    if request.method == 'POST' and form.validate():
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+        #fetch email and username to make sure the user isn't posing
+        possibleEmail = User.query.filter_by(email=email).count()
+        possibleUsername = User.query.filter_by(username=username).count()
+        if not is_email(email):
+            flash('Whoa! "' + email + '" does not seem like an email address!', 'danger')
+            return redirect('/register')
+        elif possibleEmail > 0:
+            flash('Whoa! "' + email + '" is already in use!', 'danger')
+            return redirect('/register')
+        if possibleUsername > 0:
+            flash('Whoa! "' + username + '" is already in use!', 'danger')
+            return redirect('/register')
+        if password != verify:
+            flash('Whoa! "' + username + '"Passwords do not match!', 'danger')
+            return redirect('/register')
+        #if we made it this far and they pass the tests, lettuce add them.
+        user = User(email, username, password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')#maybe to a dashboard????????
+    else:
+        return render_template('register.html', title='Register', form=form)
 
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username)
+        if password == user.password:
+            session['user'] = user.username
+            flash('welcome back, '+ user.username)
+            return redirect("/")
+        flash('bad username or password')
+        return redirect("/login")
     return render_template('login.html', title='Login', form=form)
 
 
 
 @app.route('/logout')
 def logout():
+    del session['user']
+    #return redirect("/")
     return render_template('logout.html', title='Logout')
 
 
@@ -108,6 +147,15 @@ def logout():
 def blog_entry():
     form = BlogForm(request.form)
     return render_template('blog_entry.html', title='Blog Entry', form=form)
+
+
+#list of permitted pages if not logged in session
+endpoints_without_login = ['login', 'register', 'home']
+#test before each route call to make sure user is in session
+@app.before_request
+def require_login():
+    if not ('user' in session or request.endpoint in endpoints_without_login):
+        return redirect("/register")
 
 
 
